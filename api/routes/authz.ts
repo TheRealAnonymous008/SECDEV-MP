30d14f4e2867ce256a46ef85655379df45e20668
Jacob Miguel Villa <jacob_villa@dlsu.edu.ph>
2024-06-06 17:29:42 +0800

    add: rate limit for /login

--- api/routes/authz.ts
+++ api/routes/authz.ts
@@ -2,6 +2,7 @@ import express = require('express');
 import controller from "../controllers/authz";
 import * as inputValidation from '../middleware/inputValidation';
 const router = express.Router();
+import { rateLimit } from 'express-rate-limit';
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
@@ -30,9 +31,18 @@ function validateRequestBody(req: express.Request, res: express.Response, next: 
     }
 }
 
+// Rate limiter for login capping
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minute window
+	limit: 10, // Limit each IP to 10 requests per `window` (here, per 5 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+
 router.get('/verify', controller.verify)
 router.post('/register', validateRequestBody, controller.register) 
-router.post('/login', controller.login)       
+router.post('/login', limiter, controller.login)       
 router.post('/logout', controller.logout)
 
 export default router;
