e2263a6fede2df67ae93e576e166b7ecd20dc44e
Jacob Miguel Villa <jacob_villa@dlsu.edu.ph>
2024-06-11 19:04:05 +0800

    small: fix mobile number regex

--- client/src/components/users/InputValidation.ts
+++ client/src/components/users/InputValidation.ts
@@ -2,6 +2,6 @@ export const emailRegex = /^(?!.*[-_.](?![a-zA-Z0-9]))[a-zA-Z0-9][a-zA-Z0-9._-]*
 export const nameRegex = /^[A-Z][a-zA-Z]{1,34}$/;
 export const wordRegex = /^[a-zA-Z]{1,34}$/;
 export const usernameRegex = /^[a-zA-Z0-9]{4,35}$/;
-export const mobileNumberRegex = /^09\d{11}$/;
+export const mobileNumberRegex = /^09\d{9}$/;
 export const integerRegex = /\d+$/;
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W]).{8,32}$/;
