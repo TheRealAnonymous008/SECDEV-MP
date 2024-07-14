865f939164bd81a3ca8a33032759d4a18278ec4c
Joshua Carl Ejercito <joshejercito01@gmail.com>
2024-07-14 23:07:21 +0800

    update: modal wrapper so pdf view isnt scuffy

--- client/src/components/orders/InvoiceSubform.tsx
+++ client/src/components/orders/InvoiceSubform.tsx
@@ -48,95 +48,97 @@ export const InvoiceSubform = (props: {setValue : any, errors : any, default? : 
 
     return (
         <ModalWrapper front={"Add Invoice"} isVisible={isVisible} setIsVisible={setIsVisible}>
-            <div>
+            <div style={{ maxHeight: '80vh', overflowY: 'auto', padding: '20px' }}>
+                <div>
+                    <br />
+                    <label htmlFor="invoiceAmount">Invoice Amount</label>
+                    <input type='text' name="invoice.amount" id="invoice.amount"
+                        defaultValue={invoice.amount.toString()}
+                        onChange={
+                            (e) => {setInvoice({...invoice, amount: parseFloat(e.target.value)})}
+                        }
+                    />
+                    {errors.invoice?.amount && <p>Invoice amount has wrong format</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="invoiceDeductible">Invoice Deductible</label>
+                    <input type='text' name="invoice.deductible" id="invoice.deductible"
+                        defaultValue={invoice.deductible.toString()}
+                        onChange={
+                            (e) => {setInvoice({...invoice, deductible: parseFloat(e.target.value)})}
+                        }
+                    />
+                    {errors.invoice?.deductible && <p>Deductible has wrong format</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="agentFirstName">Agent First Name</label>
+                    <input type='text' name="invoice.agentFirstName" id="invoice.agentFirstName"
+                        defaultValue={invoice.agentFirstName}
+                        onChange={
+                            (e) => {setInvoice({...invoice, agentFirstName: e.target.value})}
+                        }
+                    />
+                    {errors.invoice?.agentFirstName && <p>Agent first name has wrong format</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="agentLastName">Agent Last Name</label>
+                    <input  type='text' name="invoice.agentLastName" id="invoice.agentLastName"
+                        defaultValue={invoice.agentLastName}
+                        onChange={
+                            (e) => {setInvoice({...invoice, agentLastName: e.target.value})}
+                        }
+                    />
+                    {errors.invoice?.agentLastName && <p>Agent last name has wrong format</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="datePaid">Date Paid</label>
+                    <input  
+                        defaultValue = {
+                            invoice.datePaid ? 
+                            (invoice.datePaid.valueOf() === 0 ? "mm-dd-yyyy" : ConvertDate(invoice.datePaid)) : ""
+                        }
+                        onChange={
+                            (e) => {setInvoice({...invoice, datePaid: new Date(e.target.value)})}
+                        }
+                    type ="date" name="invoice.datePaid" id="invoice.datePaid"/>
+                    {errors.invoice?.datePaid && <p>Date is invalid</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="invoiceAgentCommision">Agent Commission</label>
+                    <input type='text' name="invoice.agentCommission" id="invoice.agentCommission"
+                        defaultValue={
+                            invoice.agentCommission.toString()
+                        }
+                        onChange={
+                            (e) => {setInvoice({...invoice, agentCommission: parseFloat(e.target.value)})}
+                        }
+                    />
+                    {errors.invoice?.agentCommission && <p>Agent Commission has wrong format</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="pdfUpload">PDF File</label>
+                    <input type="file" name="pdfFile" id="pdfUpload" accept=".pdf"
+                        onChange={handleFileChange}
+                    />
+                    {selectedPdf && <p>Selected PDF: {selectedPdf.name}</p>}
+                    {showPdfViewer && (
+                        <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '10px' }}>
+                            <iframe title="Uploaded PDF" src={pdfSrc} width="100%" height="400px"></iframe>
+                        </div>
+                    )}
+                </div>
+
                 <br />
-                <label htmlFor="invoiceAmount">Invoice Amount</label>
-                <input type='text' name="invoice.amount" id="invoice.amount"
-                    defaultValue={invoice.amount.toString()}
-                    onChange={
-                        (e) => {setInvoice({...invoice, amount: parseFloat(e.target.value)})}
-                    }
-                />
-                {errors.invoice?.amount && <p>Invoice amount has wrong format</p>}
+                <br />
+                
+                <input type='button' name="submit" onClick={onSubmit}value={"Submit"} />
             </div>
-
-            <div>
-                <label htmlFor="invoiceDeductible">Invoice Deductible</label>
-                <input type='text' name="invoice.deductible" id="invoice.deductible"
-                    defaultValue={invoice.deductible.toString()}
-                    onChange={
-                        (e) => {setInvoice({...invoice, deductible: parseFloat(e.target.value)})}
-                    }
-                />
-                {errors.invoice?.deductible && <p>Deductible has wrong format</p>}
-            </div>
-
-            <div>
-                <label htmlFor="agentFirstName">Agent First Name</label>
-                <input type='text' name="invoice.agentFirstName" id="invoice.agentFirstName"
-                    defaultValue={invoice.agentFirstName}
-                    onChange={
-                        (e) => {setInvoice({...invoice, agentFirstName: e.target.value})}
-                    }
-                />
-                {errors.invoice?.agentFirstName && <p>Agent first name has wrong format</p>}
-            </div>
-
-            <div>
-                <label htmlFor="agentLastName">Agent Last Name</label>
-                <input  type='text' name="invoice.agentLastName" id="invoice.agentLastName"
-                    defaultValue={invoice.agentLastName}
-                    onChange={
-                        (e) => {setInvoice({...invoice, agentLastName: e.target.value})}
-                    }
-                />
-                {errors.invoice?.agentLastName && <p>Agent last name has wrong format</p>}
-            </div>
-
-            <div>
-                <label htmlFor="datePaid">Date Paid</label>
-                <input  
-                    defaultValue = {
-                        invoice.datePaid ? 
-                        (invoice.datePaid.valueOf() === 0 ? "mm-dd-yyyy" : ConvertDate(invoice.datePaid)) : ""
-                    }
-                    onChange={
-                        (e) => {setInvoice({...invoice, datePaid: new Date(e.target.value)})}
-                    }
-                type ="date" name="invoice.datePaid" id="invoice.datePaid"/>
-                {errors.invoice?.datePaid && <p>Date is invalid</p>}
-            </div>
-
-            <div>
-                <label htmlFor="invoiceAgentCommision">Agent Commission</label>
-                <input type='text' name="invoice.agentCommission" id="invoice.agentCommission"
-                    defaultValue={
-                        invoice.agentCommission.toString()
-                    }
-                    onChange={
-                        (e) => {setInvoice({...invoice, agentCommission: parseFloat(e.target.value)})}
-                    }
-                />
-                {errors.invoice?.agentCommission && <p>Agent Commission has wrong format</p>}
-            </div>
-
-            <div>
-                <label htmlFor="pdfUpload">PDF File</label>
-                <input type="file" name="pdfFile" id="pdfUpload" accept=".pdf"
-                    onChange={handleFileChange}
-                />
-                {selectedPdf && <p>Selected PDF: {selectedPdf.name}</p>}
-                {showPdfViewer && (
-                    <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '10px' }}>
-                        <iframe title="Uploaded PDF" src={pdfSrc} width="100%" height="400px"></iframe>
-                    </div>
-                )}
-            </div>
-
-            <br />
-            <br />
-            
-            <input type='button' name="submit" onClick={onSubmit}value={"Submit"} />
         </ModalWrapper>
     )
 }
