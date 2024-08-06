8becc5139e3dcbe3ad454a1f9b4a02583c1538c3
Joshua Carl Ejercito <joshejercito01@gmail.com>
2024-08-06 19:44:05 +0800

    update: vehicle validation required and optional

--- api/controllers/vehicle.ts
+++ api/controllers/vehicle.ts
@@ -2,7 +2,7 @@ import  express = require('express');
 import { makeVehicleArrayView, makeVehicleView } from '../projections/vehicle';
 import { VehicleQuery, VehicleRepository } from '../repository/vehicle';
 import { VehicleRow } from '../models/vehicle';
-import { baseValidation, validateInteger, validateLicensePlate, validateLimit, validateWord } from '../middleware/inputValidation';
+import { baseValidation, validateInteger, validateLicensePlate, validateLimit, validateWord, validateRequired, validateOptional } from '../middleware/inputValidation';
 
 const all = (req: express.Request, res: express.Response, next : express.NextFunction) => {
     VehicleRepository.retrieveAll()
@@ -20,7 +20,7 @@ const all = (req: express.Request, res: express.Response, next : express.NextFun
 
 const id = (req: express.Request, res: express.Response, next : express.NextFunction) => {
     try {
-        let id = validateInteger(req.query.id.toString())
+        let id = validateRequired(req.query.id.toString(), validateInteger)
         VehicleRepository.retrieveById(id)
         .then((result) => {
             if (result.length == 0){
@@ -41,13 +41,13 @@ const id = (req: express.Request, res: express.Response, next : express.NextFunc
 const create = (req: express.Request, res: express.Response, next : express.NextFunction) => {
     try {
         const vehicle : VehicleRow = {
-            LicensePlate: validateLicensePlate(req.body.licensePlate),
-            Model: validateWord(req.body.model),
-            Manufacturer: validateWord(req.body.manufacturer),
-            YearManufactured: validateInteger(req.body.yearManufactured),
-            Color: validateWord(req.body.color),
-            Engine: validateWord(req.body.engine),
-            Remarks: baseValidation(req.body.remarks)
+            LicensePlate: validateRequired(req.body.licensePlate, validateLicensePlate),
+            Model: validateRequired(req.body.model, validateWord),
+            Manufacturer: validateRequired(req.body.manufacturer, validateWord),
+            YearManufactured: validateRequired(req.body.yearManufactured, validateInteger),
+            Color: validateRequired(req.body.color, validateWord),
+            Engine: validateRequired(req.body.engine, validateWord),
+            Remarks: validateOptional(req.body.remarks, baseValidation)
         };
         
         VehicleRepository.insert(vehicle)
@@ -69,15 +69,15 @@ const create = (req: express.Request, res: express.Response, next : express.Next
 const update = (req: express.Request, res: express.Response, next : express.NextFunction) => {
     try {
         const vehicle : VehicleRow = {
-            LicensePlate: validateLicensePlate(req.body.licensePlate),
-            Model: validateWord(req.body.model),
-            Manufacturer: validateWord(req.body.manufacturer),
-            YearManufactured: validateInteger(req.body.yearManufactured),
-            Color: validateWord(req.body.color),
-            Engine: validateWord(req.body.engine),
-            Remarks: baseValidation(req.body.remarks)
+            LicensePlate: validateRequired(req.body.licensePlate, validateLicensePlate),
+            Model: validateRequired(req.body.model, validateWord),
+            Manufacturer: validateRequired(req.body.manufacturer, validateWord),
+            YearManufactured: validateRequired(req.body.yearManufactured, validateInteger),
+            Color: validateRequired(req.body.color, validateWord),
+            Engine: validateRequired(req.body.engine, validateWord),
+            Remarks: validateOptional(req.body.remarks, baseValidation)
         };
-        let id = validateInteger(req.query.id.toString())
+        let id = validateRequired(req.query.id.toString(), validateInteger)
 
         VehicleRepository.update(id, vehicle)
             .then((result) => {
@@ -97,7 +97,7 @@ const update = (req: express.Request, res: express.Response, next : express.Next
 
 const remove = (req: express.Request, res: express.Response, next : express.NextFunction) => {
     try {
-        let id = validateInteger(req.query.id.toString())
+        let id = validateRequired(req.query.id.toString(), validateInteger)
         VehicleRepository.delete(id)
             .then((result) => {
                 if (result == undefined){
@@ -135,15 +135,15 @@ const filter = (req: express.Request, res: express.Response, next : express.Next
 
 
 const makeQuery = (req : express.Request) : VehicleQuery => {
-    const licensePlate = baseValidation(req.query.licensePlate)
-    const model = baseValidation(req.query.model)
-    const manufacturer = baseValidation(req.query.manufacturer)
-    const yearManufactured = baseValidation(req.query.yearManufactured)
-    const color = baseValidation(req.query.color)
-    const engine = baseValidation(req.query.engine)
-    const remarks = baseValidation(req.query.remarks)
-    const limit = validateLimit(req.query.limit)
-    const skip = baseValidation(req.query.skip)
+    const licensePlate = validateRequired(req.query.licensePlate, baseValidation)
+    const model = validateRequired(req.query.model, baseValidation)
+    const manufacturer = validateRequired(req.query.manufacturer, baseValidation)
+    const yearManufactured = validateRequired(req.query.yearManufactured, baseValidation)
+    const color = validateRequired(req.query.color, baseValidation)
+    const engine = validateRequired(req.query.engine, baseValidation)
+    const remarks = validateOptional(req.query.remarks, baseValidation)
+    const limit = validateRequired(req.query.limit, validateLimit)
+    const skip = validateRequired(req.query.skip, baseValidation)
 
     return {
         licensePlate: (licensePlate) ? (licensePlate as string) : null,
